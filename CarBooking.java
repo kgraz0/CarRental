2491cc8f4616e74931b93771ed9b1555267d2977
Karolis Grazulis <k.grazulis@yahoo.com>
2019-04-26 20:19:54 +0100

    Modify print layout and calculate tax function

--- CarBooking.java
+++ CarBooking.java
@@ -1,7 +1,13 @@
 public class CarBooking extends Car {
 	int numOfDays;
 	static int priceWithTax;
+	Car selectedCar;
 
+	public CarBooking(Car selectedCar, int numOfDays) {
+		this.selectedCar = selectedCar;
+		this.numOfDays = numOfDays;
+
+	}
 // setters and getters to update class values
 public int getNumOfDays() {
 	return numOfDays;
@@ -15,7 +21,19 @@ public void setNumOfDays(int numOfDays) {
 	this.numOfDays = numOfDays;
 }
 
-public static void setPriceWithTax(int basePrice, int taxRate) {
-	priceWithTax = basePrice + (basePrice * taxRate / 100);
+// set total price 
+public static void setPriceWithTax(Car selectedCar, int numOfDays) {
+	int basePrice = selectedCar.getdayBasePrice();
+	int taxRate = selectedCar.getTaxRate(); 
+
+	priceWithTax = (basePrice + (basePrice * taxRate / 100)) * numOfDays;
 }
+
+@Override
+    public String toString() {
+        return 
+        "Selected car: " + selectedCar 
+        + "Number of days: " + numOfDays + "\n"
+        + "Price with tax: " + priceWithTax;
+    }
 }
